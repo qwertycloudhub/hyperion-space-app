e2a9dde75197d2ec000f2e26ef3ac22f433521e7
Hari <harisuresh300512@gmail.com>
2024-10-06 18:26:55 -0500

    html changes

--- gen.py
+++ gen.py
@@ -3,10 +3,11 @@
 import os
 
 
-files = os.listdir("./static/imgs/mars")
+files = os.listdir("./static/imgs/lunar")
 
 mapped= []
 p = open("paste.txt", "a")
 for f in files:
-    p.write(f"<option value=\"{f}\">{f.replace(".png", "")}</option>\n")
 
+    p.write(f"<option value=\"{f}\">{f[14:24]}</option>\n")
+    #print(f[14:24])
